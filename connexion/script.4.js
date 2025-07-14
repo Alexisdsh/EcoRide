d9f0f0cbe0efeb74b50c4a522f06722e6f2e07ee
zipeik <alexisdsh89@gmail.com>
2025-07-15 00:26:15 +0200

    ajout d'une option pour cacher le mdp ou non

--- connexion/script.4.js
+++ connexion/script.4.js
@@ -5,3 +5,25 @@ function showForm(formId) {
   document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
   document.querySelector(`.tab[onclick="showForm('${formId}')"]`).classList.add('active');
 }
+
+//MDP OEIL
+document.addEventListener("DOMContentLoaded", function() {
+  const toggleIcons = document.querySelectorAll('.toggle-password');
+
+  toggleIcons.forEach(function(icon) {
+    icon.addEventListener('click', function() {
+      const inputId = this.getAttribute('data-target');
+      const input = document.getElementById(inputId);
+      if (input.type === "password") {
+        input.type = "text";
+        this.classList.remove("fa-eye");
+        this.classList.add("fa-eye-slash");
+      } else {
+        input.type = "password";
+        this.classList.remove("fa-eye-slash");
+        this.classList.add("fa-eye");
+      }
+    });
+  });
+});
+
